eb43ccef695f5c05578073a06da7a937673bb6eb
MiguelMtz-Tv <miguelayo128@gmail.com>
2023-05-03 11:07:50 -0600

    added: payment form

--- src/app/pages/pagar/pagar.component.ts
+++ src/app/pages/pagar/pagar.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./pagar.component.css']
 })
 export class PagarComponent {
-
+  cards: any = [
+    {
+      id: '1',
+      finishedIn: '4152313451205771',
+      owner: 'Miguel Martinez Castro',
+      expiration: '07/24',
+      lastFour: '5771',
+    },
+    {
+      id: '2',
+      finishedIn: '4152313451204489',
+      owner: 'Carlos Rivera Lopez',
+      expiration: '07/23',
+      lastFour: '4489',
+    },
+    {
+      id: '3',
+      finishedIn: '4152313451202213',
+      owner: 'Martín Mendez Loeza',
+      expiration: '07/21',
+      lastFour: '2213',
+    }
+  ]
 }
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -6,6 +6,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import {MatTabsModule} from '@angular/material/tabs'; 
 import {MatMenuModule} from '@angular/material/menu';
 import {MatExpansionModule} from '@angular/material/expansion'; 
+import {MatCardModule} from '@angular/material/card'; 
+import {MatRadioModule, MAT_RADIO_DEFAULT_OPTIONS} from '@angular/material/radio'; 
 
 @NgModule({
     imports:[
@@ -16,6 +18,8 @@ import {MatExpansionModule} from '@angular/material/expansion';
         MatTabsModule,
         MatMenuModule,
         MatExpansionModule,
+        MatRadioModule,
+        MatCardModule,
     ],
     exports:[
         MatButtonModule,
@@ -25,7 +29,13 @@ import {MatExpansionModule} from '@angular/material/expansion';
         MatTabsModule,
         MatMenuModule,
         MatExpansionModule,
-    ]
+        MatRadioModule,
+        MatCardModule,
+    ],
+    providers: [{
+        provide: MAT_RADIO_DEFAULT_OPTIONS,
+        useValue: { color: 'primary' },
+    }]
 })
 
 export class MaterialModule { }
